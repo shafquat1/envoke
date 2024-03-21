f9b2a635f7ebac74ff7b1b59d356ea73bdee2180
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-21 12:13:19 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/write_nfc_tag.dart
+++ lib/custom_code/actions/write_nfc_tag.dart
@@ -7,22 +7,41 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
-import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
-import 'package:ndef/ndef.dart' as ndef;
+// import 'package:ndef/ndef.dart' as ndef;
+import 'package:nfc_manager/nfc_manager.dart';
 
-Future<void> writeNfcTag(String? userId) async {
-  NFCAvailability availability;
+Future<void> writeNfcTag(String userId) async {
+  NfcManager.instance.startSession(onDiscovered: (NfcTag badge) async {
+    var ndef = Ndef.from(badge);
 
-  availability = await FlutterNfcKit.nfcAvailability;
+    if (ndef != null && ndef.isWritable) {
+      NdefRecord ndefRecord = NdefRecord.createText(userId);
+      NdefMessage message = NdefMessage([ndefRecord]);
 
-  try {
-    // Poll for the NFC tag
-    NFCTag tag = await FlutterNfcKit.poll();
-
-    if (tag.ndefWritable!) {
-      await FlutterNfcKit.writeNDEFRecords([new ndef.TextRecord(text: userId)]);
+      try {
+        await ndef.write(message);
+      } catch (e) {
+        NfcManager.instance
+            .stopSession(errorMessage: "Error while writing to badge");
+      }
     }
-  } catch (e) {
-    print('Error scanning NFC tag: $e');
-  }
+
+    NfcManager.instance.stopSession();
+  });
 }
+// Future<void> writeNfcTag(String? userId) async {
+//   NFCAvailability availability;
+
+//   availability = await FlutterNfcKit.nfcAvailability;
+
+//   try {
+//     // Poll for the NFC tag
+//     NFCTag tag = await FlutterNfcKit.poll();
+
+//     if (tag.ndefWritable!) {
+//       await FlutterNfcKit.writeNDEFRecords([new ndef.TextRecord(text: userId)]);
+//     }
+//   } catch (e) {
+//     print('Error scanning NFC tag: $e');
+//   }
+// }
