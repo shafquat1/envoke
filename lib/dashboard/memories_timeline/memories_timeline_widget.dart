7b5116fdb12d30d7c471ccf5d96b28e4c1dc492d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-26 09:35:52 +0000

    Updating to latest FlutterFlow output.

--- lib/dashboard/memories_timeline/memories_timeline_widget.dart
+++ lib/dashboard/memories_timeline/memories_timeline_widget.dart
@@ -441,6 +441,36 @@ class _MemoriesTimelineWidgetState extends State<MemoriesTimelineWidget> {
                         ),
                       ),
                     ),
+                    FFButtonWidget(
+                      onPressed: () async {
+                        GoRouter.of(context).prepareAuthEvent();
+                        await authManager.signOut();
+                        GoRouter.of(context).clearRedirectLocation();
+
+                        context.goNamedAuth('SplashScreen', context.mounted);
+                      },
+                      text: 'Logout',
+                      options: FFButtonOptions(
+                        height: 40.0,
+                        padding: const EdgeInsetsDirectional.fromSTEB(
+                            24.0, 0.0, 24.0, 0.0),
+                        iconPadding:
+                            const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                        color: FlutterFlowTheme.of(context).primary,
+                        textStyle:
+                            FlutterFlowTheme.of(context).titleSmall.override(
+                                  fontFamily: 'Istanbul type',
+                                  color: Colors.white,
+                                  useGoogleFonts: false,
+                                ),
+                        elevation: 3.0,
+                        borderSide: const BorderSide(
+                          color: Colors.transparent,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
                   ],
                 ),
               ),
