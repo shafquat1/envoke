680f1c44a1aee7addafc8d4b31b726d8fac7ccf7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-16 11:24:57 +0000

    Updating to latest FlutterFlow output.

--- lib/onboarding/signup_page/signup_page_model.dart
+++ lib/onboarding/signup_page/signup_page_model.dart
@@ -11,54 +11,19 @@ class SignupPageModel extends FlutterFlowModel<SignupPageWidget> {
   FocusNode? textFieldFocusNode1;
   TextEditingController? textController1;
   String? Function(BuildContext, String?)? textController1Validator;
-  String? _textController1Validator(BuildContext context, String? val) {
-    if (val == null || val.isEmpty) {
-      return 'Field is required';
-    }
-
-    return null;
-  }
-
   // State field(s) for TextField widget.
   FocusNode? textFieldFocusNode2;
   TextEditingController? textController2;
   String? Function(BuildContext, String?)? textController2Validator;
-  String? _textController2Validator(BuildContext context, String? val) {
-    if (val == null || val.isEmpty) {
-      return 'Field is required';
-    }
-
-    return null;
-  }
-
   // State field(s) for TextField widget.
   FocusNode? textFieldFocusNode3;
   TextEditingController? emailTextController;
   String? Function(BuildContext, String?)? emailTextControllerValidator;
-  String? _emailTextControllerValidator(BuildContext context, String? val) {
-    if (val == null || val.isEmpty) {
-      return 'Field is required';
-    }
-
-    if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
-      return 'Has to be a valid email address.';
-    }
-    return null;
-  }
-
   // State field(s) for TextField widget.
   FocusNode? textFieldFocusNode4;
   TextEditingController? passwordTextController;
   late bool passwordVisibility;
   String? Function(BuildContext, String?)? passwordTextControllerValidator;
-  String? _passwordTextControllerValidator(BuildContext context, String? val) {
-    if (val == null || val.isEmpty) {
-      return 'Field is required';
-    }
-
-    return null;
-  }
-
   // State field(s) for Checkbox widget.
   bool? checkboxValue1;
   // State field(s) for Checkbox widget.
@@ -68,11 +33,7 @@ class SignupPageModel extends FlutterFlowModel<SignupPageWidget> {
 
   @override
   void initState(BuildContext context) {
-    textController1Validator = _textController1Validator;
-    textController2Validator = _textController2Validator;
-    emailTextControllerValidator = _emailTextControllerValidator;
     passwordVisibility = false;
-    passwordTextControllerValidator = _passwordTextControllerValidator;
   }
 
   @override
