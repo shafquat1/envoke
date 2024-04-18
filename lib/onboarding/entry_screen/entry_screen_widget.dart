42c1acf2a6284f07570c1c490793a0dae93162b4
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-18 12:52:41 +0000

    Updating to latest FlutterFlow output.

--- lib/onboarding/entry_screen/entry_screen_widget.dart
+++ lib/onboarding/entry_screen/entry_screen_widget.dart
@@ -154,6 +154,7 @@ class _EntryScreenWidgetState extends State<EntryScreenWidget> {
                       ),
                     if (responsiveVisibility(
                       context: context,
+                      phone: false,
                       tablet: false,
                       tabletLandscape: false,
                       desktop: false,
